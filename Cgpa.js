b376956c888cf257768f9cc1f4fd4c720e9e5866
Swapneel Shubham <62759701+swapneel1026@users.noreply.github.com>
2022-07-04 22:22:35 +0530

    updated css

---
+++ Cgpa.js
@@ -0,0 +1,85 @@
+<<<<<<< HEAD
+import React, { useState } from "react";
+
+const Cgpa = () => {
+  const [percentage, setPercentage] = useState("");
+  const [result, setResult] = useState("Waiting for Input ");
+  const calculateCgpa = () => {
+    let newPercentage = Number(percentage);
+
+    if (newPercentage !== 0 && !isNaN(newPercentage)) {
+      setResult(`${percentage * 9.5}% `);
+    } else {
+      alert("Please Enter correct numeric value");
+    }
+  };
+  return (
+    <div className="Container">
+      <h1>CGPA Calculator</h1>
+      <div>
+        <label htmlFor="cgpaValue">CGPA: </label>
+        <input
+          id="cgpaValue"
+          type="text"
+          placeholder="Enter your CGPA"
+          onChange={(e) => {
+            setPercentage(e.target.value);
+          }}
+        />
+      </div>
+
+      <br />
+      <button onClick={calculateCgpa}>Calculate</button>
+      <div>
+        <br />
+        <span>Percentage:</span>
+        <span>{result}</span>
+      </div>
+    </div>
+  );
+};
+
+export default Cgpa;
+=======
+import React, { useState } from "react";
+
+const Cgpa = () => {
+  const [percentage, setPercentage] = useState("");
+  const [result, setResult] = useState("Waiting for Input ");
+  const calculateCgpa = () => {
+    let newPercentage = Number(percentage);
+
+    if (newPercentage !== 0 && !isNaN(newPercentage)) {
+      setResult(`${percentage * 9.5}% `);
+    } else {
+      alert("Please Enter correct numeric value");
+    }
+  };
+  return (
+    <div className="Container">
+      <h1>CGPA Calculator</h1>
+      <div>
+        <label htmlFor="cgpaValue">CGPA: </label>
+        <input
+          id="cgpaValue"
+          type="text"
+          placeholder="Enter your CGPA"
+          onChange={(e) => {
+            setPercentage(e.target.value);
+          }}
+        />
+      </div>
+
+      <br />
+      <button onClick={calculateCgpa}>Calculate</button>
+      <div>
+        <br />
+        <span>Percentage:</span>
+        <span>{result}</span>
+      </div>
+    </div>
+  );
+};
+
+export default Cgpa;
+>>>>>>> 211eabaa7b8a2d599c4c41b5fbd12fb4d97ed053
